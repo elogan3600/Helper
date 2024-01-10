552920f63adee98219698e143bcee4e164766622
elogan3600 <elogan3600@student.pps.net>
2024-01-10 10:32:34 -0800

    what

--- src/Helper.java
+++ src/Helper.java
@@ -1,3 +1,10 @@
 public class Helper {
-    
+    public static int randint(int num1, int num2) {
+        int newNum = 0;
+        System.out.println((num2 + ((num1 * -1) + 1)));
+        if (num1 < 0) {
+            newNum = (int) (Math.random() * (num2 + (num1 * -1)) + (num1 - 1));
+        }
+        return newNum;
+    }
 }
---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        int rand = 0;
+        for (int x = 0; x < 1000; x++) {
+            rand = Helper.randint(-2,3);
+            System.out.println(rand);
+        }
+        int j = 0;
+        }
+    }
