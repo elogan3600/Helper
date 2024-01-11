83f30c4754ee0aa7d29e3d481fd08e857f06ade7
elogan3600 <elogan3600@student.pps.net>
2024-01-11 09:58:00 -0800

    jvh

--- src/Helper.java
+++ src/Helper.java
@@ -1,10 +1,19 @@
 public class Helper {
     public static int randint(int num1, int num2) {
-        int newNum = 0;
-        System.out.println((num2 + ((num1 * -1) + 1)));
-        if (num1 < 0) {
-            newNum = (int) (Math.random() * (num2 + (num1 * -1)) + (num1 - 1));
-        }
+        int newNum;
+        newNum = (int) (Math.random() * (Math.abs(num2 - num1) + 1) + num1);
+        System.out.println((Math.abs(num2 - num1) + 1));
+//        if (num1 < 0) {
+//            double percent = (double) (num1 * -1) / num2;
+//            double dNum = Math.random();
+//            if (dNum < percent) {
+//                newNum = (int) (Math.random() * (num2 + 1));
+//            }
+//            else if (dNum > percent) {
+//                newNum = (int) (Math.random() * (num1 - 1) - 1);
+//            }
+//            System.out.println(percent);
+//        }
         return newNum;
     }
 }
