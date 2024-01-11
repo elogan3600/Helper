c3e4b3a98764f01e6130f86a23921ce5722aef40
elogan3600 <elogan3600@student.pps.net>
2024-01-11 10:32:12 -0800

    jvhekjl

--- src/Helper.java
+++ src/Helper.java
@@ -1,19 +1,26 @@
 public class Helper {
     public static int randint(int num1, int num2) {
-        int newNum;
-        newNum = (int) (Math.random() * (Math.abs(num2 - num1) + 1) + num1);
-        System.out.println((Math.abs(num2 - num1) + 1));
-//        if (num1 < 0) {
-//            double percent = (double) (num1 * -1) / num2;
-//            double dNum = Math.random();
-//            if (dNum < percent) {
-//                newNum = (int) (Math.random() * (num2 + 1));
-//            }
-//            else if (dNum > percent) {
-//                newNum = (int) (Math.random() * (num1 - 1) - 1);
-//            }
-//            System.out.println(percent);
+        int newNum = 0;
+        if (num1 > num2) {
+            return -1;
+        }
+        else if (num1 >= 0) {
+            newNum = (int) (Math.random() * (num2 - num1 + 1) + num1);
+        }
+//        else if (num2 < 0){
+//
 //        }
+        else {
+            double percent = (double) (num1 * -1) / num2;
+            double dNum = Math.random();
+            if (dNum < percent) {
+                newNum = (int) (Math.random() * (num2 + 1));
+            }
+            else if (dNum > percent) {
+                newNum = (int) (Math.random() * (num1 - 1) - 1);
+            }
+            //System.out.println(percent);
+        }
         return newNum;
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,23 +2,17 @@ public class Tester {
     public static void main(String[] args) {
         int rand = 0;
         int[] arr = new int[10000];
-        while (rand != 9){
-            rand = Helper.randint(-2,3);
-            //System.out.println(rand);
-
-        }
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         for (int x = 0; x < 10000; x++) {
            rand = Helper.randint(-2,3);
            arr[x] = rand;
-           //System.out.println(rand);
+           System.out.println(rand);
         }
         int sum = 0;
         for (int i : arr) {
             sum += i;
         }
         double average = (double) sum / arr.length;
-        //System.out.println(average);
+        System.out.println(average);
         int j = 0;
         }
     }
